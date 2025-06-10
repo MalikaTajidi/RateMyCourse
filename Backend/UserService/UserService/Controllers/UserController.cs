5dd404a82ecf4fbe8f10cf0314640b36d452758e
safae <labjakhsafae160@gmail.com>
2025-06-10 14:57:08 +0100

    getbyID and delete

--- Backend/UserService/UserService/Controllers/UserController.cs
+++ Backend/UserService/UserService/Controllers/UserController.cs
@@ -103,11 +103,23 @@ namespace UserService.Controllers
 
             if (professor == null)
                 return NotFound();
-
             return Ok(professor);
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
 
+            if (user == null)
+                return NotFound(); 
+
+            _context.Users.Remove(user);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
 
     }
 }
